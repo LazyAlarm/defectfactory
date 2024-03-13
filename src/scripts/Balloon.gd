c2b480c07368c0a45174cdf616b91983af49674f
LazyAlarm <91964112+LazyAlarm@users.noreply.github.com>
2024-03-12 18:03:02 -0700

    Fixed some scaling inconsistencies, adjusted overall composition and item sizing in preparation for the final models

--- src/scripts/Balloon.gd
+++ src/scripts/Balloon.gd
@@ -1,6 +1,9 @@
 extends Item
 class_name Balloon
 
+@onready var scale_point = $ScalePivot
+
+
 var min_scale : float = 0.5
 var max_scale : float = 2
 var accuracy_threshold : float = 0.05
@@ -8,20 +11,19 @@ var accuracy_threshold : float = 0.05
 
 func aberrate():
 	var determiner : int = randi_range(0,4)
-	if determiner == 4: scale = Vector3.ONE * randf_range(1.3, 2)
-	else: scale = Vector3.ONE * randf_range(0.5, 0.8)
-
+	if determiner == 4: scale_point.scale = Vector3.ONE * randf_range(1.3, 2)
+	else: scale_point.scale = Vector3.ONE * randf_range(0.5, 0.8)
 
 
 func _process(delta):
 	super._process(delta)
 	if !selected: return
 	var input : float = Input.get_axis("down","up")
-	scale += Vector3.ONE * input * delta
-	if scale.x > max_scale:
-		scale = Vector3.ONE * max_scale
-	elif scale.x < min_scale:
-		scale = Vector3.ONE * min_scale
-	if abs(1 - scale.x) < accuracy_threshold:
-		scale = Vector3.ONE
+	scale_point.scale += Vector3.ONE * input * delta
+	if scale_point.scale.x > max_scale:
+		scale_point.scale = Vector3.ONE * max_scale
+	elif scale_point.scale.x < min_scale:
+		scale_point.scale = Vector3.ONE * min_scale
+	if abs(1 - scale_point.scale.x) < accuracy_threshold:
+		scale_point.scale = Vector3.ONE
 		on_corrected()
--- src/scripts/prism.gd
+++ src/scripts/prism.gd
@@ -15,8 +15,8 @@ var speed : float = 20:
 
 func aberrate():
 	var determiner : int = randi_range(0,1)
-	if determiner == 1: rotate_z(deg_to_rad(randf_range(20,max_angle)))
-	else: rotate_z(deg_to_rad(randf_range(-max_angle,-20)))
+	if determiner == 1: tilt.rotate_z(deg_to_rad(randf_range(20,max_angle)))
+	else: tilt.rotate_z(deg_to_rad(randf_range(-max_angle,-20)))
 	speed = randi_range(0,5)
 
 func _process(delta):
@@ -24,11 +24,11 @@ func _process(delta):
 	mesh.rotate_y(delta * speed)
 	if !selected: return
 	speed += Input.get_action_strength("up")
-	if abs(rad_to_deg(rotation.z)) < accuracy_threshold:
-		rotation.z = 0
+	if abs(rad_to_deg(tilt.rotation.z)) < accuracy_threshold:
+		tilt.rotation.z = 0
 		if speed == max_speed:
 			on_corrected()
 	else:
-		rotate_z(Input.get_axis("left","right") * -delta)
-		rotation.z = clamp(rotation.z,deg_to_rad(-max_angle),deg_to_rad(max_angle))
+		tilt.rotate_z(Input.get_axis("left","right") * -delta)
+		tilt.rotation.z = clamp(tilt.rotation.z,deg_to_rad(-max_angle),deg_to_rad(max_angle))
 
